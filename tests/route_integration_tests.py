c351d7a7bff99f1c6283861483975a7b3d449cf1
josh <jh@semetrical.com>
2022-02-15 14:51:04 +0000

    Added filters to our auth tests and moved them to the Acceptance Tests

--- tests/route_integration_tests.py
+++ tests/route_integration_tests.py
@@ -14,155 +14,6 @@ class TestConfig(Config):
     API_DOMAIN = 'http://127.0.0.1:5000'
 
 
-class Authentication_Integration_Tests(unittest.TestCase):
-
-    def setUp(self):
-        self.app = create_app(TestConfig)
-        self.app_context = self.app.app_context()
-        self.app_context.push()
-        self.app = self.app.test_client()
-        db.create_all()
-        mock_data_generation.insert_appointments_and_therapists(db)
-        mock_data_generation.insert_api_users(db)
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-        self.app_context.pop()
-
-    def test_can_generate_access_token_and_refresh_token_for_username_and_password_and_then_access_api(self):
-        """Tests that given a prexisting username and password an access token and refreshToken can be generated via graphQL Mutation
-           Value of the access token and refresh token are different each time function is called. W
-        """
-
-        endpoint = f'{TestConfig.API_DOMAIN}/graphql'
-        response = self.app.post(endpoint, json={"query": """
-            mutation{auth(password:"password",username:"test_user"){
-              accessToken
-              refreshToken}
-            }
-        """})
-        self.assertEqual(response.status_code, 200)
-
-        access_token_generated = response.json['data']['auth']['accessToken']
-
-        response = self.app.post(endpoint, headers={"authorization": f"Bearer {access_token_generated}"}, json={"query": """
-                    {
-                  appointments {
-                    edges {
-                      node {
-                        startTimeUnixSeconds
-                        durationSeconds
-                      }
-                    }
-                  }
-                }
-          """})
-
-        self.assertEqual(response.json, {
-            "data": {
-                "appointments": {
-                    "edges": [
-                        {
-                            "node": {
-                                "startTimeUnixSeconds": 1644747572,
-                                "durationSeconds": 3600
-                            }
-                        },
-                        {
-                            "node": {
-                                "startTimeUnixSeconds": 1644780000,
-                                "durationSeconds": 3600
-                            }
-                        }
-                    ]
-                }
-            }
-        })
-
-    def test_unauthorized_requests_are_rejected(self):
-        """Tests that given a prexisting username and password an access token and refreshToken can be generated via graphQL Mutation
-           Value of the access token and refresh token are different each time function is called. W
-        """
-
-        endpoint = f'{TestConfig.API_DOMAIN}/graphql'
-
-        response = self.app.post(endpoint, headers={"authorization": f"Bearer InvalidToken"}, json={"query": """
-                    {
-                      appointments {
-                        edges {
-                          node {
-                            startTimeUnixSeconds
-                            durationSeconds
-                          }
-                        }
-                      }
-                    }
-             """})
-
-        self.assertEqual(response.json, {'errors': [
-            {'message': 'Not enough segments', 'locations': [{'line': 3, 'column': 23}], 'path': ['appointments']}],
-            'data': {'appointments': None}})
-
-    def test_access_token_can_be_refreshed_and_then_access_api(self):
-        # gen access token
-
-        endpoint = f'{TestConfig.API_DOMAIN}/graphql'
-        response = self.app.post(endpoint, json={"query": """
-                    mutation{auth(password:"password",username:"test_user"){
-                      accessToken
-                      refreshToken}
-                    }
-                """})
-
-        access_token_generated = response.json['data']['auth']['accessToken']
-        refresh_token_generated = response.json['data']['auth']['refreshToken']
-
-        # generate refresh token
-        response = self.app.post(endpoint,
-                                 json={
-                                     "query": 'mutation{refresh(refreshToken:"' + refresh_token_generated + '"){newToken}}'})
-
-        new_access_token_generated = response.json['data']['refresh']['newToken']
-        self.assertNotEqual(access_token_generated, new_access_token_generated)
-
-        # make a protected request with new access token
-        final_request = self.app.post(endpoint, headers={"authorization": f"Bearer {new_access_token_generated}"},
-                                      json={"query": """
-                                   {
-                                      appointments {
-                                        edges {
-                                          node {
-                                            startTimeUnixSeconds
-                                            durationSeconds
-                                          }
-                                        }
-                                      }
-                                    }
-                            """})
-
-        self.assertEqual(final_request.status_code, 200)
-        self.assertEqual(final_request.json, {
-            "data": {
-                "appointments": {
-                    "edges": [
-                        {
-                            "node": {
-                                "startTimeUnixSeconds": 1644747572,
-                                "durationSeconds": 3600
-                            }
-                        },
-                        {
-                            "node": {
-                                "startTimeUnixSeconds": 1644780000,
-                                "durationSeconds": 3600
-                            }
-                        }
-                    ]
-                }
-            }
-        })
-
 
 class API_Integration_Tests(unittest.TestCase):
 
@@ -238,6 +89,7 @@ class API_Acceptance_Tests(unittest.TestCase):
         db.create_all()
         # generate our mock data
         mock_data_generation.insert_appointments_and_therapists(db)
+        mock_data_generation.insert_api_users(db)
 
     def tearDown(self):
         db.session.remove()
@@ -422,8 +274,6 @@ class API_Acceptance_Tests(unittest.TestCase):
             }
         })
 
-
-
     @mock.patch('API.authentication.decorators._extract_header_token_value')
     @mock.patch('API.authentication.decorators.verify_jwt_in_argument')
     def test_appointment_mutate_on_graphql_endpoint(self, *args):
@@ -602,6 +452,190 @@ class API_Acceptance_Tests(unittest.TestCase):
                             }
                         }
                     }
+                }
+            }
+        })
+
+    def test_can_generate_access_token_and_refresh_token_for_username_and_password_and_then_query_api_with_filters(
+            self):
+        """Tests that given a prexisting username and password an access token and refreshToken can be generated via graphQL Mutation
+           Value of the access token and refresh token are different each time function is called. W
+        """
+
+        endpoint = f'{TestConfig.API_DOMAIN}/graphql'
+        response = self.app.post(endpoint, json={"query": """
+            mutation{auth(password:"password",username:"test_user"){
+              accessToken
+              refreshToken}
+            }
+        """})
+        self.assertEqual(response.status_code, 200)
+
+        access_token_generated = response.json['data']['auth']['accessToken']
+
+        response = self.app.post(endpoint, headers={"authorization": f"Bearer {access_token_generated}"}, json={"query": """
+                    {
+          appointments(filters: {hasSpecialisms: ["ADHD"], type: "one-off", startTimeUnixSecondsRange: {begin: 1644747500, end: 1644790000}}) {
+            edges {
+              node {
+                therapists {
+                  firstName
+                  lastName
+                  specialisms {
+                    edges {
+                      node {
+                        specialismName
+                      }
+                    }
+                  }
+                }
+                startTimeUnixSeconds
+                durationSeconds
+                type
+              }
+            }
+          }
+        }
+          """})
+
+        self.assertEqual(response.json, {
+            "data": {
+                "appointments": {
+                    "edges": [
+                        {
+                            "node": {
+                                "therapists": {
+                                    "firstName": "jeff",
+                                    "lastName": "smith",
+                                    "specialisms": {
+                                        "edges": [
+                                            {
+                                                "node": {
+                                                    "specialismName": "Addiction"
+                                                }
+                                            },
+                                            {
+                                                "node": {
+                                                    "specialismName": "ADHD"
+                                                }
+                                            }
+                                        ]
+                                    }
+                                },
+                                "startTimeUnixSeconds": 1644747572,
+                                "durationSeconds": 3600,
+                                "type": "one-off"
+                            }
+                        }
+                    ]
+                }
+            }
+        })
+
+    def test_unauthorized_requests_are_rejected(self):
+        """Tests that given a prexisting username and password an access token and refreshToken can be generated via graphQL Mutation
+           Value of the access token and refresh token are different each time function is called. W
+        """
+
+        endpoint = f'{TestConfig.API_DOMAIN}/graphql'
+
+        response = self.app.post(endpoint, headers={"authorization": f"Bearer InvalidToken"}, json={"query": """
+                    {
+                      appointments {
+                        edges {
+                          node {
+                            startTimeUnixSeconds
+                            durationSeconds
+                          }
+                        }
+                      }
+                    }
+             """})
+
+        self.assertEqual(response.json, {'errors': [
+            {'message': 'Not enough segments', 'locations': [{'line': 3, 'column': 23}], 'path': ['appointments']}],
+            'data': {'appointments': None}})
+
+    def test_access_token_can_be_refreshed_and_then_the_api_queries_with_filters(self):
+        # gen access token
+
+        endpoint = f'{TestConfig.API_DOMAIN}/graphql'
+        response = self.app.post(endpoint, json={"query": """
+                    mutation{auth(password:"password",username:"test_user"){
+                      accessToken
+                      refreshToken}
+                    }
+                """})
+
+        access_token_generated = response.json['data']['auth']['accessToken']
+        refresh_token_generated = response.json['data']['auth']['refreshToken']
+
+        # generate refresh token
+        response = self.app.post(endpoint,
+                                 json={
+                                     "query": 'mutation{refresh(refreshToken:"' + refresh_token_generated + '"){newToken}}'})
+
+        new_access_token_generated = response.json['data']['refresh']['newToken']
+        self.assertNotEqual(access_token_generated, new_access_token_generated)
+
+        # make a protected request with new access token
+        final_request = self.app.post(endpoint, headers={"authorization": f"Bearer {new_access_token_generated}"},
+                                      json={"query": """
+                                  {
+                                      appointments(filters: {hasSpecialisms: ["ADHD"], type: "one-off", startTimeUnixSecondsRange: {begin: 1644747500, end: 1644790000}}) {
+                                        edges {
+                                          node {
+                                            therapists {
+                                              firstName
+                                              lastName
+                                              specialisms {
+                                                edges {
+                                                  node {
+                                                    specialismName
+                                                  }
+                                                }
+                                              }
+                                            }
+                                            startTimeUnixSeconds
+                                            durationSeconds
+                                            type
+                                          }
+                                        }
+                                      }
+                                    }
+                            """})
+
+        self.assertEqual(final_request.status_code, 200)
+        self.assertEqual(final_request.json, {
+            "data": {
+                "appointments": {
+                    "edges": [
+                        {
+                            "node": {
+                                "therapists": {
+                                    "firstName": "jeff",
+                                    "lastName": "smith",
+                                    "specialisms": {
+                                        "edges": [
+                                            {
+                                                "node": {
+                                                    "specialismName": "Addiction"
+                                                }
+                                            },
+                                            {
+                                                "node": {
+                                                    "specialismName": "ADHD"
+                                                }
+                                            }
+                                        ]
+                                    }
+                                },
+                                "startTimeUnixSeconds": 1644747572,
+                                "durationSeconds": 3600,
+                                "type": "one-off"
+                            }
+                        }
+                    ]
                 }
             }
         })
